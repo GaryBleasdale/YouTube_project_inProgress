045cc868be360df44484cfc2c52fb2a0c27e4d9a
Gary Malcolm Bleasdale <g.m.bleasdale@gmail.com>
2022-02-16 12:08:16 -0300

    improved style, added JS and CSS to improve appearance

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-require('dotenv').config();
-let api_key=process.env.API_KEY;
+
 let container = document.body.querySelector('.container');
 let searchResults = document.body.querySelector('.search-results');
 let searchBtn=document.body.querySelector('#searchbtn')
@@ -17,22 +16,26 @@ function getVal() {
   val = document.querySelector('input').value;
   String(val)
   let results = document.querySelectorAll('.search-result')
+ 
   if(results.length>0){
     for (i=0;i<results.length;i++){
       results[i].remove();
     }
   }
-  let resultText = document.createElement("h3")
+  let resultText = document.createElement("h3");
+  resultText.classList.add('yousearched')
   resultText.innerText=`
   You just searched for "${val}"
   `
 
-  searchResultText.appendChild(resultText)
+  
+  setTimeout(function(){searchResultText.appendChild(resultText)},1000)
+  
 
 
 
 $.ajax({
-  url: `https://www.googleapis.com/youtube/v3/search?part=snippet&%20&q=${val}%20&type=video%20&key=${api_key}`,
+  url: `https://www.googleapis.com/youtube/v3/search?part=snippet&%20&q=${val}%20&type=video%20&key=AIzaSyBMccR4yASb2d9yTvk-wKBPbS6776uxWGA`,
   success: function (data) {
     JSON.stringify(data)
     console.log(data)
@@ -41,7 +44,7 @@ $.ajax({
       result.innerHTML = `
     <a href="https://www.youtube.com/watch?v=${data.items[i].id.videoId}">
     <div class='left-hand'>
-    <img src='${data.items[i].snippet.thumbnails.default.url}'/>
+    <img src='${data.items[i].snippet.thumbnails.high.url}'/>
     </div>
     <div class='right-hand'>
     <h2>${data.items[i].snippet.title}</h2>
@@ -59,36 +62,30 @@ $.ajax({
 });
 
 }
+let chevron1 = document.body.querySelector('.chevron1')
+let chevron2 = document.body.querySelector('.chevron2')
+let chevron3 = document.body.querySelector('.chevron3')
+let animate = document.body.querySelector('.animate')
+
+searchBtn.addEventListener('mouseover', function(){
+  chevron2.style.display='inline';
+  chevron3.style.display='inline';
+  chevron1.classList.add('animate');
+  setTimeout(function(){chevron2.classList.add('animate')},75);
+  setTimeout(function(){chevron3.classList.add('animate')},150);
+;
+})
+
+searchBtn.addEventListener('mouseout', function(){
+  chevron2.style.display='none';
+  chevron2.classList.remove('animate');
+  chevron3.style.display='none';
+  chevron3.classList.remove('animate')
+  chevron2.style.display='none';
+  chevron1.classList.remove('animate');
+})
 
 
-// function getNext() {
-  
-//   let results = document.querySelectorAll('.search-result')
-//   if(results.length>0){
-//     for (i=0;i<results.length;i++){
-//       results[i].remove();
-//     }
-    
-//   }
-// $.ajax({
-//   url: `https://www.googleapis.com/youtube/v3/search?part=snippet&%20pageToken=${data.nextPageToken}&q=${val}%20&type=video%20&key=AIzaSyBMccR4yASb2d9yTvk-wKBPbS6776uxWGA`,
-//   success: function (data) {
-//     JSON.stringify(data)
-//     data.items.forEach(function(element, i){
-//       let result = document.createElement("div")
-//       result.innerHTML = `
-//     <a href="https://www.youtube.com/watch?v=${data.items[i].id.videoId}">
-//     <img src='${data.items[i].snippet.thumbnails.default.url}'/>
-//     <h2>${data.items[i].snippet.title}</h2>
-//     <p>${data.items[i].snippet.description}</p>
-//     </a>
-//     `
-//     result.classList.add('search-result')  
-//     container.appendChild(result)
-//   })
-//   }
-// });
 
-// }
 
 
